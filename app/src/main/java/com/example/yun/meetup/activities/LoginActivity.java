f6c9d84cb10a6d7820e4c666f495e1082b0d6263
danielmagalhaesit <danielmagalhaesit@gmail.com>
2017-11-28 16:29:41 -0500

    login and registration screens are done

---
+++ app/src/main/java/com/example/yun/meetup/activities/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.yun.meetup.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.yun.meetup.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
