b2a697178f60d611de2172637dff180cda822351
danielmagalhaesit <danielmagalhaesit@gmail.com>
2017-11-28 17:09:03 -0500

    Login, registration, and Main screens are done

---
+++ app/src/main/java/com/example/yun/meetup/activities/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.yun.meetup.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.yun.meetup.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
