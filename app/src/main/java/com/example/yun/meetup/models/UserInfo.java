57f971bf7901af53aad5b2c97de3093f6b123b36
alessioRM87 <alessio.iannella@gmail.com>
2017-12-03 01:08:21 -0500

    // USER INFO
    Added model user info

---
+++ app/src/main/java/com/example/yun/meetup/models/UserInfo.java
@@ -0,0 +1,45 @@
+package com.example.yun.meetup.models;
+
+/**
+ * Created by alessio on 02-Dec-17.
+ */
+
+public class UserInfo {
+
+    private String email;
+    private String password;
+    private String firstName;
+    private String lastName;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
