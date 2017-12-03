33b34c7f9458407ede4134a9a5745b7d399fec8a
alessioRM87 <alessio.iannella@gmail.com>
2017-12-03 01:09:49 -0500

    // MAIN ACTIVITY
    Layout and welcome message

--- app/src/main/java/com/example/yun/meetup/activities/MainActivity.java
+++ app/src/main/java/com/example/yun/meetup/activities/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.yun.meetup.activities;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
 import com.example.yun.meetup.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textViewMainTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textViewMainTitle = (TextView) findViewById(R.id.textViewMainTitle);
+
+        SharedPreferences sharedPref = this.getSharedPreferences("userInfo", Context.MODE_PRIVATE);
+        textViewMainTitle.setText("Welcome, " + sharedPref.getString("name", "") + "!");
+
     }
 }
