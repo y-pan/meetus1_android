c08e122f0a97a0731bf52aa475b9c7b1800e10e6
alessioRM87 <alessio.iannella@gmail.com>
2017-12-05 19:22:29 -0500

    // GROUP
    Added group model

---
+++ app/src/main/java/com/example/yun/meetup/models/Group.java
@@ -0,0 +1,80 @@
+package com.example.yun.meetup.models;
+
+/**
+ * Created by alessio on 05-Dec-17.
+ */
+
+public class Group {
+    private String ID;
+    private String title;
+    private String description;
+    private String subtitle;
+    private String hostID;
+    private float latitude;
+    private float longitude;
+    private String date;
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getSubtitle() {
+        return subtitle;
+    }
+
+    public void setSubtitle(String subtitle) {
+        this.subtitle = subtitle;
+    }
+
+    public String getHostID() {
+        return hostID;
+    }
+
+    public void setHostID(String hostID) {
+        this.hostID = hostID;
+    }
+
+    public float getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(float latitude) {
+        this.latitude = latitude;
+    }
+
+    public float getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(float longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
