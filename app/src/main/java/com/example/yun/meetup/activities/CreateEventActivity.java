72bfc0cecdb7efd23b7875ffad283fde8e488e71
danielmagalhaesit <danielmagalhaesit@gmail.com>
2017-12-06 12:58:19 -0500

    create an event screen is done

--- app/src/main/java/com/example/yun/meetup/activities/CreateEventActivity.java
+++ app/src/main/java/com/example/yun/meetup/activities/CreateEventActivity.java
@@ -44,7 +44,6 @@ public class CreateEventActivity extends AppCompatActivity {
         edtDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 new DatePickerDialog(CreateEventActivity.this, date, calendar
                         .get(Calendar.YEAR), calendar.get(Calendar.MONTH),
                         calendar.get(Calendar.DAY_OF_MONTH)).show();
