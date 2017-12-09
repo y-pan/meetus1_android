d152ede805d1c29f4568a5a87748e15993414991
alessioRM87 <alessio.iannella@gmail.com>
2017-12-08 20:27:00 -0500

    // API PROVIDER
    Get body as string, removed try/catch and added throws

--- app/src/main/java/com/example/yun/meetup/providers/ApiProvider.java
+++ app/src/main/java/com/example/yun/meetup/providers/ApiProvider.java
@@ -23,7 +23,7 @@ public class ApiProvider {
 
     private static final String BASE_URL = "https://meetus01.herokuapp.com/api";
 
-    public String sendRequest(String absoluteURL, String method, JSONObject body){
+    public String sendRequest(String absoluteURL, String method, String body) throws  IOException {
 
         // If provided URL is partial, append internal API BASE_URL prefix
         String urlPath = absoluteURL.startsWith("http") ? absoluteURL : getAbsoluteUrl(absoluteURL);
@@ -33,61 +33,34 @@ public class ApiProvider {
         BufferedWriter bufferedWriter = null;
         BufferedReader bufferedReader = null;
 
-        try{
-            // Initialize and config request, then connect to server
-            URL url = new URL(urlPath);
-            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-            urlConnection.setRequestMethod(method);
-            urlConnection.setRequestProperty("Content-Type", "application/json");
-            urlConnection.connect();
+        // Initialize and config request, then connect to server
+        URL url = new URL(urlPath);
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        urlConnection.setRequestMethod(method);
+        urlConnection.setRequestProperty("Content-Type", "application/json");
+        urlConnection.connect();
 
-            //Write data into server
-            if(body != null) {
-                OutputStream outputStream = urlConnection.getOutputStream();
-                bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream));
-                bufferedWriter.write(body.toString());
-                bufferedWriter.flush();
-            }
+        //Write data into server
+        if(body != null) {
+            OutputStream outputStream = urlConnection.getOutputStream();
+            bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream));
+            bufferedWriter.write(body);
+            bufferedWriter.flush();
+        }
 
-            // read response from server
-            InputStream inputStream = urlConnection.getInputStream();
-            bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-            String line;
-            while ((line = bufferedReader.readLine()) != null) {
-                result.append(line).append("\n");
-            }
+        // read response from server
+        InputStream inputStream = urlConnection.getInputStream();
+        bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+        String line;
+        while ((line = bufferedReader.readLine()) != null) {
+            result.append(line).append("\n");
         }
-        catch (ProtocolException e) {
-            e.printStackTrace();
-            result = null;
+
+        if(bufferedReader != null){
+            bufferedReader.close();
         }
-        catch (MalformedURLException e) {
-            e.printStackTrace();
-            result = null;
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-            result = null;
-        }
-        finally {
-            if(bufferedReader != null){
-                try {
-                    bufferedReader.close();
-                }
-                catch (IOException e) {
-                    e.printStackTrace();
-                    return null;
-                }
-            }
-            if(bufferedWriter != null){
-                try {
-                    bufferedWriter.close();
-                }
-                catch (IOException e) {
-                    e.printStackTrace();
-                    return null;
-                }
-            }
+        if(bufferedWriter != null){
+            bufferedWriter.close();
         }
 
         return result.toString();
