66a9edf796936a7d09ee4d40088e99c676c8bb11
alessioRM87 <alessio.iannella@gmail.com>
2017-12-08 21:14:53 -0500

    // CREATE EVENT REQUEST
    Created object for create event request

---
+++ app/src/main/java/com/example/yun/meetup/requests/CreateEventRequest.java
@@ -0,0 +1,81 @@
+package com.example.yun.meetup.requests;
+
+/**
+ * Created by alessio on 08-Dec-17.
+ */
+
+public class CreateEventRequest {
+
+    private String host_id;
+    private String title;
+    private String description;
+    private String subtitle;
+    private float latitude;
+    private float longitude;
+    private String date;
+    private String address;
+
+    public String getHost_id() {
+        return host_id;
+    }
+
+    public void setHost_id(String host_id) {
+        this.host_id = host_id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getSubtitle() {
+        return subtitle;
+    }
+
+    public void setSubtitle(String subtitle) {
+        this.subtitle = subtitle;
+    }
+
+    public float getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(float latitude) {
+        this.latitude = latitude;
+    }
+
+    public float getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(float longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
