b95d0cb2e8762a23dd3b0d4b807ad1446a469c4b
alessioRM87 <alessio.iannella@gmail.com>
2017-12-08 21:15:07 -0500

    // EVENT
    Changed some field names

--- app/src/main/java/com/example/yun/meetup/models/Event.java
+++ app/src/main/java/com/example/yun/meetup/models/Event.java
@@ -5,92 +5,16 @@ package com.example.yun.meetup.models;
  */
 
 public class Event {
-    private String ID;
+    private String _id;
     private String title;
     private String description;
     private String subtitle;
-    private String hostID;
+    private String host_id;
     private UserInfo userInfo;
     private float latitude;
     private float longitude;
     private String address;
     private String date;
 
-    public String getID() {
-        return ID;
-    }
 
-    public void setID(String ID) {
-        this.ID = ID;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getSubtitle() {
-        return subtitle;
-    }
-
-    public void setSubtitle(String subtitle) {
-        this.subtitle = subtitle;
-    }
-
-    public String getHostID() {
-        return hostID;
-    }
-
-    public void setHostID(String hostID) {
-        this.hostID = hostID;
-    }
-
-    public UserInfo getUserInfo(){ return userInfo; }
-
-    public void setUserInfo(UserInfo userInfo){
-        this.userInfo = userInfo;
-    }
-
-    public float getLatitude() {
-        return latitude;
-    }
-
-    public void setLatitude(float latitude) {
-        this.latitude = latitude;
-    }
-
-    public float getLongitude() {
-        return longitude;
-    }
-
-    public void setLongitude(float longitude) {
-        this.longitude = longitude;
-    }
-
-    public String getDate() {
-        return date;
-    }
-
-    public void setDate(String date) {
-        this.date = date;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
 }
