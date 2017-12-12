98be781f8be4cd54caf8833f57897a7132659431
alessioRM87 <alessio.iannella@gmail.com>
2017-12-11 23:46:41 -0500

    // EVENT LIST ACTIVITY
    Call network manager to get list of hosted events

--- app/src/main/java/com/example/yun/meetup/activities/EventListActivity.java
+++ app/src/main/java/com/example/yun/meetup/activities/EventListActivity.java
@@ -1,7 +1,11 @@
 package com.example.yun.meetup.activities;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -10,20 +14,29 @@ import android.widget.ListView;
 
 import com.example.yun.meetup.R;
 import com.example.yun.meetup.adapters.EventListViewAdapter;
+import com.example.yun.meetup.managers.NetworkManager;
+import com.example.yun.meetup.models.APIResult;
 import com.example.yun.meetup.models.Event;
 import com.example.yun.meetup.models.UserInfo;
+import com.example.yun.meetup.requests.EventListRequest;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class EventListActivity extends AppCompatActivity {
 
-    ListView listView;
+    private ListView listViewEvents;
+    private ConstraintLayout constraintLayoutEventListLoading;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_list);
+
+        listViewEvents = (ListView) findViewById(R.id.listViewEvents);
+        constraintLayoutEventListLoading = (ConstraintLayout) findViewById(R.id.constraintLayoutEventListLoading);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         toolbar.setTitle("Event List");
@@ -41,55 +54,81 @@ public class EventListActivity extends AppCompatActivity {
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-//          DUMMY DATA
+////          DUMMY DATA
+//
+//        Event[] eventList = new Event[]{
+//                new Event(),
+//                new Event(),
+//                new Event(),
+//                new Event(),
+//                new Event()
+//        };
+//
+//        UserInfo userInfo;
+//        userInfo = new UserInfo();
+//        userInfo.setName("Nice Professor");
+//
+//        eventList[0].set_id("111");
+//        eventList[0].setTitle("Nice Project Class");
+//        eventList[0].setUserInfo(userInfo);
+//        eventList[0].setDate("01/01/2013");
+//        eventList[0].setAddress("941 Progress Ave, Toronto, ON M1K 5E9");
+//        eventList[1].set_id("222");
+//        eventList[1].setTitle("Nice Project Class");
+//        eventList[1].setUserInfo(userInfo);
+//        eventList[1].setDate("01/01/2013");
+//        eventList[1].setAddress("941 Progress Ave, Toronto, ON M1K 5E9");
+//        eventList[2].set_id("333");
+//        eventList[2].setTitle("Nice Project Class");
+//        eventList[2].setUserInfo(userInfo);
+//        eventList[2].setDate("01/01/2013");
+//        eventList[2].setAddress("941 Progress Ave, Toronto, ON M1K 5E9");
+//        eventList[3].set_id("444");
+//        eventList[3].setTitle("Nice Project Class");
+//        eventList[3].setUserInfo(userInfo);
+//        eventList[3].setDate("01/01/2013");
+//        eventList[3].setAddress("941 Progress Ave, Toronto, ON M1K 5E9");
+//        eventList[4].set_id("555");
+//        eventList[4].setTitle("Nice Project Class");
+//        eventList[4].setUserInfo(userInfo);
+//        eventList[4].setDate("01/01/2013");
+//        eventList[4].setAddress("941 Progress Ave, Toronto, ON M1K 5E9");
+//
+//        // DUMMY DATA END
 
-        Event[] eventList = new Event[]{
-                new Event(),
-                new Event(),
-                new Event(),
-                new Event(),
-                new Event()
-        };
+        EventListRequest eventListRequest = new EventListRequest();
 
-        UserInfo userInfo;
-        userInfo = new UserInfo();
-        userInfo.setName("Nice Professor");
+        SharedPreferences sharedPreferences = getSharedPreferences("userInfo", Context.MODE_PRIVATE);
+        eventListRequest.setHost_id(sharedPreferences.getString("id", null));
 
-        eventList[0].set_id("111");
-        eventList[0].setTitle("Nice Project Class");
-        eventList[0].setUserInfo(userInfo);
-        eventList[0].setDate("01/01/2013");
-        eventList[0].setAddress("941 Progress Ave, Toronto, ON M1K 5E9");
-        eventList[1].set_id("222");
-        eventList[1].setTitle("Nice Project Class");
-        eventList[1].setUserInfo(userInfo);
-        eventList[1].setDate("01/01/2013");
-        eventList[1].setAddress("941 Progress Ave, Toronto, ON M1K 5E9");
-        eventList[2].set_id("333");
-        eventList[2].setTitle("Nice Project Class");
-        eventList[2].setUserInfo(userInfo);
-        eventList[2].setDate("01/01/2013");
-        eventList[2].setAddress("941 Progress Ave, Toronto, ON M1K 5E9");
-        eventList[3].set_id("444");
-        eventList[3].setTitle("Nice Project Class");
-        eventList[3].setUserInfo(userInfo);
-        eventList[3].setDate("01/01/2013");
-        eventList[3].setAddress("941 Progress Ave, Toronto, ON M1K 5E9");
-        eventList[4].set_id("555");
-        eventList[4].setTitle("Nice Project Class");
-        eventList[4].setUserInfo(userInfo);
-        eventList[4].setDate("01/01/2013");
-        eventList[4].setAddress("941 Progress Ave, Toronto, ON M1K 5E9");
-
-        // DUMMY DATA END
-
-        listView = (ListView) findViewById(R.id.lv_events);
-
-        EventListViewAdapter adapter = new EventListViewAdapter(Arrays.asList(eventList), getApplicationContext());
-
-        listView.setAdapter(adapter);
+        new EventListTask().execute(eventListRequest);
 
 
+    }
+
+    private class EventListTask extends AsyncTask<EventListRequest, Void, APIResult>{
+
+        @Override
+        protected APIResult doInBackground(EventListRequest... eventListRequests) {
+            NetworkManager networkManager = new NetworkManager();
+            return networkManager.getHostedEvents(eventListRequests[0]);
+        }
+
+        @Override
+        protected void onPostExecute(APIResult apiResult) {
+
+            if (apiResult.isResultSuccess()){
+
+                List<Event> listEvents = (List<Event>) apiResult.getResultEntity();
+
+                EventListViewAdapter adapter = new EventListViewAdapter(listEvents, getApplicationContext());
+
+                listViewEvents.setAdapter(adapter);
+
+                constraintLayoutEventListLoading.setVisibility(View.GONE);
+
+            }
+        }
     }
 
 }
