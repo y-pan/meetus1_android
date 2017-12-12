a918a6eac04a22b0e0da00e69b1f4a32f21cd1fc
alessioRM87 <alessio.iannella@gmail.com>
2017-12-11 23:45:33 -0500

    // EVENT LIST REQUEST

---
+++ app/src/main/java/com/example/yun/meetup/requests/EventListRequest.java
@@ -0,0 +1,18 @@
+package com.example.yun.meetup.requests;
+
+/**
+ * Created by alessio on 11-Dec-17.
+ */
+
+public class EventListRequest {
+
+    private String host_id;
+
+    public String getHost_id() {
+        return host_id;
+    }
+
+    public void setHost_id(String host_id) {
+        this.host_id = host_id;
+    }
+}
