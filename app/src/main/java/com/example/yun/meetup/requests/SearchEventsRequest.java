ed0e4d29f89a24770fbfd1893506b856f1ac9239
alessioRM87 <alessio.iannella@gmail.com>
2017-12-16 19:10:55 -0500

    // SEARCH EVENTS REQUEST

---
+++ app/src/main/java/com/example/yun/meetup/requests/SearchEventsRequest.java
@@ -0,0 +1,36 @@
+package com.example.yun.meetup.requests;
+
+/**
+ * Created by alessio on 16-Dec-17.
+ */
+
+public class SearchEventsRequest {
+
+    private double latitude = 0;
+    private double longitude = 0;
+    private double distance = 50;
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
+}
