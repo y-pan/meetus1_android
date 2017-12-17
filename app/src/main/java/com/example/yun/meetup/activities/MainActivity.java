b38097e488412eac51ec4087e6bf85b8e6db934a
alessioRM87 <alessio.iannella@gmail.com>
2017-12-16 19:10:12 -0500

    // MAIN ACTIVITY
    Get current location

--- app/src/main/java/com/example/yun/meetup/activities/MainActivity.java
+++ app/src/main/java/com/example/yun/meetup/activities/MainActivity.java
@@ -1,18 +1,62 @@
 package com.example.yun.meetup.activities;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
 import com.example.yun.meetup.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.LatLng;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView textViewMainTitle;
+
+    private ConstraintLayout constraintLayoutMainLoading;
+
+    private static final long LOCATION_REFRESH_TIME = 1;
+    private static final float LOCATION_REFRESH_DISTANCE = 1;
+
+    private Location currentLocation;
+
+    private LocationManager locationManager;
+
+    private final LocationListener mLocationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(final Location location) {
+            if (currentLocation == null){
+                MainActivity.this.constraintLayoutMainLoading.setVisibility(View.GONE);
+                currentLocation = new Location(location);
+            }
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +65,54 @@ public class MainActivity extends AppCompatActivity {
 
         textViewMainTitle = (TextView) findViewById(R.id.textViewMainTitle);
 
+        constraintLayoutMainLoading = (ConstraintLayout) findViewById(R.id.constraintLayoutMainLoading);
+
         SharedPreferences sharedPref = getSharedPreferences("userInfo", Context.MODE_PRIVATE);
         textViewMainTitle.setText("Welcome, " + sharedPref.getString("name", "") + "!");
 
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 200);
+            return;
+        }
+        else{
+            locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, LOCATION_REFRESH_TIME, LOCATION_REFRESH_DISTANCE, mLocationListener);
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case 200: {
+                if(grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                            && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        // TODO: Consider calling
+                        //    ActivityCompat#requestPermissions
+                        // here to request the missing permissions, and then overriding
+                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                        //                                          int[] grantResults)
+                        // to handle the case where the user grants the permission. See the documentation
+                        // for ActivityCompat#requestPermissions for more details.
+                        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 200);
+                        return;
+                    }
+                    else{
+                        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+                        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, LOCATION_REFRESH_TIME, LOCATION_REFRESH_DISTANCE, mLocationListener);
+                    }
+                }
+            }
+        }
     }
 
     public void handleOnClickLogout(View view) {
@@ -45,5 +134,15 @@ public class MainActivity extends AppCompatActivity {
     public void handleOnClickHostedEvents(View view) {
         Intent intent = new Intent(this, EventListActivity.class);
         startActivity(intent);
+    }
+
+    public void handleOnClickSearchEvent(View view) {
+        Intent intent = new Intent(this, SearchEventsActivity.class);
+        intent.putExtra("currentLatitude", currentLocation.getLatitude());
+        intent.putExtra("currentLongitude", currentLocation.getLongitude());
+        startActivity(intent);
+    }
+
+    public void handleOnClickMySubscribedEvents(View view) {
     }
 }
